86ac90ded60c7fe15902800d9119a6a27e30d442
Elena <elena_feuerstein@yahoo.de>
2019-07-05 14:37:23 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,20 @@
+﻿ using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+   
+   public PlayerMovement movement;
+   
+   
+   //This function runs when we hit another object
+   //We get Information about the collision and call it "collisionInfo"
+   void OnCollisionEnter(Collision collisionInfo ){
+        
+        //We check if the object we collided with has a tag called "Hinderniss"
+        if (collisionInfo.collider.tag == "Hinderniss"){
+            movement. enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+            
+        }
+   }
+}
