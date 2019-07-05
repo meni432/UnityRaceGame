8049c67f9071b709ddb5b42811a89153984fd67f
Elena <elena_feuerstein@yahoo.de>
2019-07-05 15:34:31 +0200

    Start und Endscreen erstellt
    Ein Start und Endscreen wurde mit Buttons erstellt.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+   //Startscreen mit Button erscheint
+   public void StartGame(){
+    
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   
+   }
+}
